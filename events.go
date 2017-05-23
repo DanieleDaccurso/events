d57e5c2c69cc9e5dacdf35c10a14cd94fa567011
Daniele D'Accurso <daccurda@googlemail.com>
2017-05-24 00:19:06 +0200

    [WIP] multiple events with same priority

--- events.go
+++ events.go
@@ -1,17 +1,17 @@
 package events
 
 import (
-	"sort"
 	"reflect"
+	"sort"
 )
 
-type Event interface {}
+type Event interface{}
 
 type onReturnFN func([]reflect.Value)
 
 // DispatchEvents will dispatch a map of events, by their int values, with an empty callback.
 // If you need to process the result of your events, consider using DispatchEventsCallback
-func DispatchEvents(evList map[int]Event, withContext interface{} )  {
+func DispatchEvents(evList map[int]Event, withContext interface{}) {
 	DispatchEventsCallback(evList, withContext, func(e []reflect.Value) {})
 }
 
@@ -40,4 +40,43 @@ func DispatchEventsCallback(evList map[int]Event, withContext interface{}, callb
 			callback(ret)
 		}
 	}
+}
+
+type ecitem struct {
+	event    *Event
+	priority int
+}
+
+type sorted_ecitem []ecitem
+
+func (a sorted_ecitem) Len() int           { return len(a) }
+func (a sorted_ecitem) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a sorted_ecitem) Less(i, j int) bool { return a[i].priority < a[j].priority }
+
+type EventCollection struct {
+	events []*ecitem
+	hp     int
+}
+
+func (e *EventCollection) AddEvent(ev *Event, prio int) {
+	e.events = append(e.events, &ecitem{ev, prio})
+	e.setHighestPrio(prio)
+}
+
+func (e *EventCollection) AppendEvent(ev *Event) {
+	e.events = append(e.events, &ecitem{ev, e.hp + 1})
+	e.setHighestPrio(e.hp + 1)
+}
+
+func (e *EventCollection) getSorted() []*ecitem {
+}
+
+func (e *EventCollection) setHighestPrio(prio int) {
+	if e.hp < prio {
+		e.hp = prio
+	}
+}
+
+func DispatchCollectionCallback(col *EventCollection, callback onReturnFN) {
+
 }
